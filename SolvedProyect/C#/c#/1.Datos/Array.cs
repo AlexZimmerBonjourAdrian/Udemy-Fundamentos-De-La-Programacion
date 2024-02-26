f080a7442415825b5243eea50c581f55000445c5
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2024-02-26 15:55:10 -0300

    c# Classes

---
+++ SolvedProyect/C#/c#/1.Datos/Array.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Array()
+{
+    private int[] array_number = [0,1,2,3,4,5];
+    public void array()
+    {
+        for(int i = 0; i<= array_number.Length -1 ; i++)
+        {
+            
+            Console.WriteLine(array_number[i]);
+        }
+    }
+}
+
+
